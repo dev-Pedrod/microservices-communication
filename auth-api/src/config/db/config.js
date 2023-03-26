12f26bc27de94b301aeaa76f028c28a72e6432a1
dev-pedrod <pedroolivxira@gmail.com>
2023-03-26 12:12:30 -0300

    feat(Auth): config sequelize

---
+++ auth-api/src/config/db/config.js
@@ -0,0 +1,23 @@
+import {Sequelize} from "sequelize";
+
+const sequelize = new Sequelize("auth-db", "postgres", "123456", {
+    host: 'localhost',
+    dialect: 'postgres',
+    quoteIdentifiers: false,
+    define: {
+        syncOnAssociation: true,
+        timestamps: false,
+        underscored: true,
+        underscoredAll: true,
+        freezeTableName: true,
+    }
+});
+
+sequelize.authenticate().then(() => {
+    console.info("Connection has ben stablished!")
+}).catch((err) => {
+    console.error("Unable connect to the database.")
+    console.error(err.message)
+});
+
+export default sequelize;
