dcea2fd25d4d115664ed5e25350dfb5c30c319ad
dev-pedrod <pedroolivxira@gmail.com>
2023-03-29 06:28:51 -0300

    feat(Auth): Find user by email

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,15 +1,21 @@
 import express from "express"
 import * as db from "./src/config/db/initial-data.js"
+import userRouter from "./src/modules/user/routes/UserRoutes.js";
+import UserService from "./src/modules/user/service/UserService.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
-db.createInitialData().then(r => {
+db.createInitialData().then(() => {
     console.info("Initial data has ben created.")
 });
 
+app.use(express.json())
+app.use("/api", userRouter);
+
 app.get('/api/status', (req, res) => {
+    UserService.findByEmail(req)
     return res.json({
         service: 'auth-api',
         status: 'up',
