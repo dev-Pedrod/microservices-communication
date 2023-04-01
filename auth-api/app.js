a9bebb13997367c7c652f2503e1a4f0bc14ecbcb
dev-pedrod <pedroolivxira@gmail.com>
2023-04-01 08:57:00 -0300

    feat(Auth): authorization

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import * as db from "./src/config/db/initial-data.js"
 import userRouter from "./src/modules/user/routes/UserRoutes.js";
-import UserService from "./src/modules/user/service/UserService.js";
+import checkToken from "./src/config/auth/check-token.js";
 
 const app = express();
 const env = process.env;
@@ -13,7 +13,7 @@ db.createInitialData().then(() => {
 
 app.use(express.json())
 app.use("/api", userRouter);
-
+app.use(checkToken)
 app.get('/api/status', (req, res) => {
     return res.json({
         service: 'auth-api',
