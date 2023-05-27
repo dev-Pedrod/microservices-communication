ddfff73dd32bc55eb496eda1dc774ef56c8581fc
dev-pedrod <pedroolivxira@gmail.com>
2023-05-27 14:46:36 -0300

    feat(Generics): Generic service implementation

--- product-api/src/main/java/com/devpedrod/productapi/modules/shared/service/IGenericSerice.java
+++ product-api/src/main/java/com/devpedrod/productapi/modules/shared/service/IGenericSerice.java
@@ -7,14 +7,14 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface IGenericSerice<T extends BaseEntity, DTO extends BaseDTO> {
-    T findById(Long id);
-    T findById(Long id, boolean includeDisabled);
+public interface IGenericSerice<T extends BaseEntity, DTO extends BaseDTO, ID> {
+    T findById(ID id, boolean includeDisabled);
+    T findById(ID id);
     Page<DTO> findAll(Pageable pageable);
     List<T> findAll();
-    T getReference(Long id);
-    T create(DTO dto);
-    T update(DTO dto);
-    void delete(Long id);
+    T getReference(ID id);
+    T create(T dto);
+    T update(T dto);
+    void delete(ID id);
     T saveAndFlush(T entity);
 }
