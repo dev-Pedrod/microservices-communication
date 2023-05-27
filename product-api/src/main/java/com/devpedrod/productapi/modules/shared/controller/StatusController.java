e743ff6f58d5f732b61eb81c0ffe556eed6a65eb
dev-pedrod <pedroolivxira@gmail.com>
2023-05-27 14:49:14 -0300

    rebase status controler

--- product-api/src/main/java/com/devpedrod/productapi/modules/shared/controller/StatusController.java
+++ product-api/src/main/java/com/devpedrod/productapi/modules/shared/controller/StatusController.java
@@ -1,6 +1,5 @@
 package com.devpedrod.productapi.modules.shared.controller;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,7 @@ import java.util.HashMap;
 public class StatusController {
 
     @GetMapping
-    public ResponseEntity<HashMap<String, Object>> apiStatus(Pageable pageable) {
+    public ResponseEntity<HashMap<String, Object>> apiStatus() {
         var response = new HashMap<String, Object>();
         response.put("service", "product-api");
         response.put("status", "up");
