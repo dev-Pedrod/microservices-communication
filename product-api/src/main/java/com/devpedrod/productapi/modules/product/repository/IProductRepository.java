648e6f7ad0b24383377e05eebb2c4af713bb72d1
dev-pedrod <pedroolivxira@gmail.com>
2023-06-25 11:40:27 -0300

    feat(Product): implementing methods to find and delete product

--- product-api/src/main/java/com/devpedrod/productapi/modules/product/repository/IProductRepository.java
+++ product-api/src/main/java/com/devpedrod/productapi/modules/product/repository/IProductRepository.java
@@ -4,6 +4,13 @@ import com.devpedrod.productapi.modules.product.model.Product;
 import com.devpedrod.productapi.modules.shared.repository.IGenericRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IProductRepository extends IGenericRepository<Product, Long> {
+    List<Product> findByNameIgnoreCaseContaining(String name);
+    List<Product> findByCategoryId(Long category_id);
+    List<Product> findBySupplierId(Long supplier_id);
+    Boolean existsByCategoryId(Long category_id);
+    Boolean existsBySupplierId(Long supplier_id);
 }
--- product-api/src/main/java/com/devpedrod/productapi/modules/product/service/IProductService.java
+++ product-api/src/main/java/com/devpedrod/productapi/modules/product/service/IProductService.java
@@ -1,10 +1,16 @@
 package com.devpedrod.productapi.modules.product.service;
 
+import com.devpedrod.productapi.modules.product.DTO.ProductResponse;
+import com.devpedrod.productapi.modules.product.DTO.ProductSalesResponse;
 import com.devpedrod.productapi.modules.product.model.Product;
-import com.devpedrod.productapi.modules.shared.DTO.BaseDTO;
 import com.devpedrod.productapi.modules.shared.service.IGenericSerice;
 
-public interface IProductService extends IGenericSerice<Product, BaseDTO, Long> {
+import java.util.List;
 
-
+public interface IProductService extends IGenericSerice<Product, ProductResponse, Long> {
+    List<ProductResponse> findByName(String name);
+    List<ProductResponse> findBySupplierId(Long supplierId);
+    List<ProductResponse> findByCategoryId(Long categoryId);
+    ProductSalesResponse findProductSales(Long id);
+    ProductResponse findByIdResponse(Long id);
 }
