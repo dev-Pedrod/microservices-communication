e4b5e5e6ff9c36a08e353b5095f58522155fadb2
dev-pedrod <pedroolivxira@gmail.com>
2023-07-22 15:06:26 -0300

    feat(Auth): update app.js and endpoint to create initial data

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,28 +1,47 @@
 import express from "express"
-import * as db from "./src/config/db/initial-data.js"
-import userRouter from "./src/modules/user/routes/UserRoutes.js";
-import checkToken from "./src/config/auth/check-token.js";
+import {createInitialData} from "./src/config/db/initial-data.js";
+import tracing from "./src/config/tracing.js";
+import userRoutes from "./src/modules/user/routes/UserRoutes.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+const CONTAINER_ENV = "container";
 
-db.createInitialData().then(() => {
-    console.info("Initial data has ben created.")
+app.get("/", (req, res) => {
+    return res.status(200).json(getOkResponse());
 });
 
-app.use(express.json())
+app.get("/api/status", (req, res) => {
+    return res.status(200).json(getOkResponse());
+});
 
-app.get('/api/status', (req, res) => {
-    return res.json({
-        service: 'auth-api',
-        status: 'up',
-        httpStatus: 200
-    })
-})
+function getOkResponse() {
+    return {
+        service: "Auth-API",
+        status: "up",
+        httpStatus: 200,
+    }
+}
 
-app.use("/api", userRouter);
+app.use(express.json());
+
+startApplication();
+
+function startApplication() {
+    if (env.NODE_ENV !== CONTAINER_ENV) {
+        createInitialData();
+    }
+}
+
+app.get("/api/initial-data", (req, res) => {
+    createInitialData();
+    return res.json({message: "Data created."});
+});
+
+app.use(tracing);
+app.use(userRoutes);
 
 app.listen(PORT, () => {
-    console.info(`Server started successfully at port: ${PORT}`);
-})
+    console.info(`Server started successfully at port ${PORT}`);
+});
--- auth-api/src/modules/user/routes/UserRoutes.js
+++ auth-api/src/modules/user/routes/UserRoutes.js
@@ -1,13 +1,15 @@
-import { Router } from "express";
+import {Router} from "express";
 
 import UserController from "../controller/UserController.js";
 import checkToken from "../../../config/auth/check-token.js";
 
 const router = new Router();
 
-router.post("/user/auth", UserController.getAccessToken);
-router.use(checkToken)
-router.get("/user/email/:email", UserController.findByEmail);
+router.post("/api/user/auth", UserController.getAccessToken);
+
+router.use(checkToken);
+
+router.get("/api/user/email/:email", UserController.findByEmail);
 
 
 export default router;
