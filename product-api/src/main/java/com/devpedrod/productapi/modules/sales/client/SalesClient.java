796325d28406c5b2f03583e06ff6dd7f73215bc8
dev-pedrod <pedroolivxira@gmail.com>
2023-07-23 18:48:25 -0300

    fix(Sales): SalesClient correction and creation of DTOs for returns

--- product-api/src/main/java/com/devpedrod/productapi/modules/sales/client/SalesClient.java
+++ product-api/src/main/java/com/devpedrod/productapi/modules/sales/client/SalesClient.java
@@ -1,18 +1,18 @@
 package com.devpedrod.productapi.modules.sales.client;
 
+import com.devpedrod.productapi.modules.sales.DTO.SalesProductResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
-import java.util.List;
 import java.util.Optional;
 
 @HttpExchange("/api/orders")
 public interface SalesClient {
 
     @GetExchange("/product/{productId}")
-    Optional<List<String>> findSalesByProductId(@PathVariable Long productId,
-                                                @RequestHeader(name = "Authorization") String authorization,
-                                                @RequestHeader(name = "transactionId") String transactionId);
+    Optional<SalesProductResponse> findSalesByProductId(@PathVariable Long productId,
+                                                        @RequestHeader(name = "Authorization") String authorization,
+                                                        @RequestHeader(name = "transactionid") String transactionId);
 }
